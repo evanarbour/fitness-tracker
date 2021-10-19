a93cc31662bb9d88d69c694b0e4a561b69ed6c9b
Evan Arbour <evan.arbour@gmail.com>
2021-10-19 09:54:55 -0700

    created html-routes.js

---
+++ routes/html-routes.js
@@ -0,0 +1,23 @@
+// Import Express router
+const router = require("express").Router();
+
+// Import path
+const path = require("path");
+
+// GET Request - Home page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..public/index.html"));
+});
+
+// GET Request - Exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "..public/exercise.html"));
+});
+
+// GET Request - Stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "..public/stats.html"));
+});
+
+module.exports = router;
+
